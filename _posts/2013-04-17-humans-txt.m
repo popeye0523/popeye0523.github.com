71606272f0f8d7587f50bc1b1ef6da7661374ba6
popeye0523 <popeye0523@sina.com>
2013-04-18 20:55:27 +0800

    Create 2013-04-17-humans-txt.m

---
+++ _posts/2013-04-17-humans-txt.m
@@ -0,0 +1,35 @@
+---
+layout: post
+title: "humans.txt: We are people, not machines"
+description: ""
+category: blog
+tags: [humans.txt, 署名, 人情味, 互联网3.0时代]
+date: 2013-04-17
+---
+{% include JB/setup %}
+## 网络是为人设置的
+“网络是为人设置的”，然我们经常这样说，但是在网站内我们只会附加一个专为搜索引擎准备的的robots.txt文件。为什么不为我们自己也配备一个文件？这也就是这个小小的文本文件存在的意义。
+
+## humans.txt是什么
+![署上自己的名字](http://humanstxt.org/img/Illustration-home/03-where-is-located.png)
+给自己的作品署名是人类的天性，然而网站业主不一定乐意让网站程序开发者/运营团队在网页上写上自己的署名，但也许他们并不反对在网站根目录里面保存一份txt文本文件？这并不会影响整个网站的浏览，相反，它可以体现网站业主们对于网站程序开发者们的尊重和感谢。
+
+## humans.txt和robots.txt
+![humans.txt和robots.txt](http://humanstxt.org/img/Illustration-home/07-the-internet-is-for-humans.png)
+请不要把humans.txt和robots.txt当成同一种东西，后者是搜索引擎抓取协议，而前者则纯粹是人，请注意，是希望在冰冷的网络代码中寻求更多乐趣和温暖的人们，互联网3.0时代进行的富有人情味的探索。　humans.txt不是什么协议，也没有任何的代码功能，它只是一个单纯的文本文件，用来保存网站建设者们那些富有人情味的信息。在百度搜索引擎上可以找到humans.txt提倡者网站，tilte里面直接写着“我们是人，不是机器”“we are people, not machines”的提倡宗旨。
+
+## humans.txt和meta信息
+![humans.txt和meta信息](http://humanstxt.org/img/Illustration-home/06-meta-name-or-humans-txt.png)
+humans.txt和meta并不是一种斗争，而是一种扩大作者信息的补充，
+
+## 开始署名吧
+![署上自己的名字](http://humanstxt.org/img/Illustration-home/05-who-should-i-mention.png)
+虽然只是一个倡议，却也是一个充满人情味的尝试。meta信息也好，about页面也好，humans.txt也好，一个比一个详细，一个比一个注重人。
+humans.txt倡议者网站建议，在这个小文件里面写上整个网站工作团队，包括网站程序设计、网页美工师、搜索引擎优化、搜索引擎营销、社会化媒体优化……名单是无止境的，很多人可以参与一个网站的创建（当然要经过本人同意），对曾经帮助过网站建设的人们致以谢意，还可以写上网站的程序版本、开发工具等等。
+即便如此，humans.txt其实没有固定的书写套路，与其说是书写，不如说是写作，站长们尽可以将自己的个人特色发挥到淋漓尽致，也可以对整个网站建设者团队进行描述……一切都由你，就算把它写成征婚、交友广告，或者是冷笑话大全，一切都由你。
+
+## 说明
+七拼八凑抄来的，希望表达清楚意思即可，也算是为了humans.txt的推广尽一份力了，人和人才越来越重的年代，一份温暖可能比多涨点钱还让人感激。
+
+内容摘自百度百科[humans.txt](http://baike.baidu.com/view/9318819.htm)  
+本文图片引用自[humans.txt提倡者网站](http://humanstxt.org/ZH)  
